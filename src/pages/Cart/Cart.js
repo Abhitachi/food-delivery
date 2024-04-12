e2ba8012cdcfd672b5ea9991af7f89967bbb26b4
Abhitachi <abhishekshetter1999@gmail.com>
2024-04-12 23:33:38 +0530

    final update

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -56,7 +56,9 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+              <b>
+                ₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
           <button onClick={() => navigate("/order")}>
